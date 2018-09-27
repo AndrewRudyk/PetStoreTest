04fbddfb3362e60b4666f889addb05241885468d
Andrey Rudyk <rudik.a.v@gmail.com>
2018-09-27 23:29:37 +0300

    Add: pod Alamofire, NetworkService

---
+++ PetStore/Services/NetworkService.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkService.swift
+//  PetStore
+//
+//  Created by Prostor9 on 27.09.2018.
+//  Copyright © 2018 me. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "http://api-dev.cuspychatstories.com/v1"
+}
+
+
+class NetworkService {
+    
+    
+    
+    
+}
